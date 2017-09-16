ebf8d541ef90bb9b55a900cc1ec7dbf105e3b04b
Tiago Coelho <tiagocoelho33@gmail.com>
2017-09-16 16:00:04 -0300

    z

--- frontend/app/leading-form/leading-form.component.js
+++ frontend/app/leading-form/leading-form.component.js
@@ -11,30 +11,16 @@ angular.
         var leadingFormController = this;
 
         leadingFormController.user = null;
-        
-        //Verifica se a palavra é palindromo.
-        function isPalindrome() {
-            var novaWord= {word: '', result: ''};
 
-            if(leadingFormController.palindromeWord == null || leadingFormController.palindromeWord == ''){
-              console.log('O campo word está vazio, por favor insira um valor valido.');
-            }else{
-              $http.get('http://localhost:3333/api/v0/palindrome/'+leadingFormController.palindromeWord).then(function(response) {
-                novaWord.word = leadingFormController.palindromeWord;
-                novaWord.result = "IS A PALINDROME"
-                leadingFormController.wordsUsed.push(novaWord);
+        //Verifica se a palavra é palindromo.
+        function sendUser() {
+              $http.post('http://localhost:3003/participantes/'+user).then(function(response) {
+                // leadingFormController.wordsUsed.push(novaWord);s
               },
               //onErro
               function(response){
-                if(response.status == 400){
-                  novaWord.word = leadingFormController.palindromeWord;
-                  novaWord.result = "IS NOT A PALINDROME"
-                  leadingFormController.wordsUsed.push(novaWord);
-                }else{
                   console.log('Um erro inesperado ocorreu. Informe o administrador.');
-                }
               }); 
-            }
         };
 
         function teste() {
