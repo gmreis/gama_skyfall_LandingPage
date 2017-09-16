c42558f37d42b7bffb56ddc686b16f67f9f42061
Geraldo Mateus Reis <reis.geraldomateus@gmail.com>
2017-09-16 16:00:39 -0300

    Commit

--- backend/api/participante/participanteService.js
+++ backend/api/participante/participanteService.js
@@ -5,12 +5,15 @@ const server = require('../../config/server')
 // POST /participante
 function addParticipante(req, res) {
 
+console.log("Parametros");
+console.log(req.params.nome);
+
   var participante = new Participante({
-    nome: "Geraldo",
-    email: "email",
-    profissao: "profissao",
-    exGama: 1,
-    exLewagon: 0
+    nome: req.params.nome,
+    email: req.params.email,
+    profissao: req.params.profissao,
+    exGama: req.params.exgama,
+    exLewagon: req.params.exlewagon
   });
 
   Participante.create(participante, function(err, result) {
